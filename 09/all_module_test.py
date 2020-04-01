f515b46c0b17c46640401e5b681ce6b25660f77d
zhy <xingzhishangyang@163.com>
2020-04-01 10:05:41 +0800

    package

---
+++ 09/all_module_test.py
@@ -0,0 +1,5 @@
+# 导入all_module模块中的所有成员
+from all_module import *
+hello()
+world()
+test()  # 会提示找不到test()函数
---
+++ 09/fk_module_test1.py
@@ -0,0 +1,5 @@
+import fk_module
+print('========')
+# 打印fk_module类型
+print(type(fk_module))
+print(fk_module)
