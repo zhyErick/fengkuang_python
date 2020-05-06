6f12961e70d0d150a509fe952fb488f4900bb6c0
zhanghaiyang <xingzhishangyang@163.com>
2020-05-06 21:05:47 +0800

    os path test

--- 12/12.3/os.path_test.py
+++ 12/12.3/os.path_test.py
@@ -2,8 +2,16 @@ from pathlib import *
 import fnmatch
 
 # 遍历当前目录下的所有文件和子目录
-for file in Path('../').iterdir():
+for file in Path('.').iterdir():
     # 访问所有以_test.py结尾的文件
-    print(file)
     if fnmatch.fnmatch(file, '*_test.py'):
         print(file)
+
+names = ['a.py', 'b.py', 'c.py', 'd.py']
+# 对names列表进行过滤
+sub = fnmatch.filter(names, '[ac].py')
+print(sub)
+
+print(fnmatch.translate('?.py'))
+print(fnmatch.translate('[ac].py'))
+print(fnmatch.translate('[a-c].py'))
